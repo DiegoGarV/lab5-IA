7c3657c0ed50791caccb5c5472c4cf0bd7ec3c8e
DiegoGarV <dgv3105@gmail.com>
2025-02-19 18:36:24 -0600

    cambios en la lectura

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 
-def cargar_imagen(ruta):
-    # Cargar la imagen
-    imagen = Image.open(ruta)
+def representar_discreta_con_color(imagen, factor_redimension=10):
+    # Redimensionar la imagen a un tamaño más pequeño (para pixelearla)
+    ancho, alto = imagen.size
+    imagen_pequeña = imagen.resize((ancho // factor_redimension, alto // factor_redimension), Image.NEAREST)
     
-    # Pasar a escala de grises
-    imagen_gray = imagen.convert('L')
-    
-    # Mostrar imagenes
-    plt.figure(figsize=(10, 5))
-    
-    # Imagen original
-    plt.subplot(1, 2, 1)
-    plt.title('Imagen Original')
-    plt.imshow(imagen)
-    plt.axis('off')
-    
-    # Imagen en escala de grises
-    plt.subplot(1, 2, 2)
-    plt.title('Escala de Grises')
-    plt.imshow(imagen_gray, cmap='gray')
-    plt.axis('off')
-    
-    plt.show()
-    
-    return imagen_gray
+    # Redimensionarla de nuevo a su tamaño original para obtener el efecto pixeleado
+    imagen_pixeleada = imagen_pequeña.resize((ancho, alto), Image.NEAREST)
 
+    imagen_suavizada = imagen_pequeña.resize((ancho, alto), Image.BOX)
+    
+    return imagen_suavizada
 
 
 ruta_imagen = 'laberintos/laberinto1.bmp'
-imagen_gray = cargar_imagen(ruta_imagen)
+imagen = imagen = Image.open(ruta_imagen)
 
+# Llamar a la función de discresión
+imagen_pixeleada_con_color = representar_discreta_con_color(imagen)
+
+# Mostrar las imagenes
+plt.figure(figsize=(10, 5))
+
+# Imagen original
+plt.subplot(1, 2, 1)
+plt.title('Imagen Original')
+plt.imshow(imagen)
+plt.axis('off')
+
+# Imagen pixeleada
+plt.subplot(1, 2, 2)
+plt.title('Imagen Pixeleada con Colores')
+plt.imshow(imagen_pixeleada_con_color)
+plt.axis('off')
+
+plt.show()
