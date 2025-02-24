7d7de1d03851a0d9e4aee44354e7a80d130f1b7f
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2025-02-23 18:34:53 -0600

    chore: clean code

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 
+
 def representar_discreta_con_color(imagen, factor_redimension=10):
     # Redimensionar la imagen a un tamaño más pequeño (para pixelearla)
     ancho, alto = imagen.size
-    imagen_pequeña = imagen.resize((ancho // factor_redimension, alto // factor_redimension), Image.NEAREST)
-    
+    imagen_pequeña = imagen.resize(
+        (ancho // factor_redimension, alto // factor_redimension), Image.NEAREST
+    )
+
     # Redimensionarla de nuevo a su tamaño original para obtener el efecto pixeleado
     imagen_pixeleada = imagen_pequeña.resize((ancho, alto), Image.NEAREST)
 
     imagen_suavizada = imagen_pequeña.resize((ancho, alto), Image.BOX)
-    
+
     return imagen_suavizada
 
 
-ruta_imagen = 'laberintos/laberinto1.bmp'
+ruta_imagen = "laberintos/laberinto1.bmp"
 imagen = imagen = Image.open(ruta_imagen)
 
 # Llamar a la función de discresión
@@ -25,14 +28,14 @@ plt.figure(figsize=(10, 5))
 
 # Imagen original
 plt.subplot(1, 2, 1)
-plt.title('Imagen Original')
+plt.title("Imagen Original")
 plt.imshow(imagen)
-plt.axis('off')
+plt.axis("off")
 
 # Imagen pixeleada
 plt.subplot(1, 2, 2)
-plt.title('Imagen Pixeleada con Colores')
+plt.title("Imagen Pixeleada con Colores")
 plt.imshow(imagen_pixeleada_con_color)
-plt.axis('off')
+plt.axis("off")
 
 plt.show()
