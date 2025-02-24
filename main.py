1dfa46930de8b9b94418f57c8b08c0622bd36bfb
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2025-02-24 13:05:04 -0600

    feat: mazeProblem class

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
-from PIL import Image
-import matplotlib.pyplot as plt
+from imageScanner import process_image
+from problem import MazeProblem
+from search import bfs, dfs, a_star, manhattan_heuristic
+from visualize import visualize_solution
 
 
-def representar_discreta_con_color(imagen, factor_redimension=10):
-    # Redimensionar la imagen a un tamaño más pequeño (para pixelearla)
-    ancho, alto = imagen.size
-    imagen_pequeña = imagen.resize(
-        (ancho // factor_redimension, alto // factor_redimension), Image.NEAREST
-    )
+def main():
+    labs = [
+        "./laberintos/laberinto1.bmp",
+        "./laberintos/laberinto2.bmp",
+        "./laberintos/laberinto3.bmp",
+    ]
 
-    # Redimensionarla de nuevo a su tamaño original para obtener el efecto pixeleado
-    imagen_pixeleada = imagen_pequeña.resize((ancho, alto), Image.NEAREST)
+    for image_path in labs:
+        print(f"\nProcesando: {image_path}")
 
-    imagen_suavizada = imagen_pequeña.resize((ancho, alto), Image.BOX)
+        maze, start, goals = process_image(image_path)
 
-    return imagen_suavizada
+        problem = MazeProblem(maze, start, goals)
+
+        solution = a_star(problem, manhattan_heuristic)
+
+        if solution:
+            visualize_solution(maze, solution, start, goals, image_path)
+        else:
+            print(f"No se encontró solución para: {image_path}")
 
 
-ruta_imagen = "laberintos/laberinto1.bmp"
-imagen = imagen = Image.open(ruta_imagen)
-
-# Llamar a la función de discresión
-imagen_pixeleada_con_color = representar_discreta_con_color(imagen)
-
-# Mostrar las imagenes
-plt.figure(figsize=(10, 5))
-
-# Imagen original
-plt.subplot(1, 2, 1)
-plt.title("Imagen Original")
-plt.imshow(imagen)
-plt.axis("off")
-
-# Imagen pixeleada
-plt.subplot(1, 2, 2)
-plt.title("Imagen Pixeleada con Colores")
-plt.imshow(imagen_pixeleada_con_color)
-plt.axis("off")
-
-plt.show()
+if __name__ == "__main__":
+    main()
